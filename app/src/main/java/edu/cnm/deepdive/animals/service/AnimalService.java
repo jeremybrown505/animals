ebe184522c9f61248425af992bdb4f49bfae5875
Jeremy Brown <Jeremy505brown@gmail.com>
2020-06-11 17:01:46 -0600

    added the ability to rotate device and whats on the screen.

--- app/src/main/java/edu/cnm/deepdive/animals/service/AnimalService.java
+++ app/src/main/java/edu/cnm/deepdive/animals/service/AnimalService.java
@@ -1,17 +1,50 @@
 package edu.cnm.deepdive.animals.service;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import edu.cnm.deepdive.animals.BuildConfig;
 import edu.cnm.deepdive.animals.model.Animal;
+import io.reactivex.Single;
 import java.util.List;
-import retrofit2.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import okhttp3.logging.HttpLoggingInterceptor.Level;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
 
 public interface AnimalService {
-
-  @POST("getAnimals")
+  static AnimalService getInstance() {
+    return InstanceHolder.INSTANCE;
+  }
   @FormUrlEncoded
-  Call<List<Animal>> getAnimals(@Field("key") String key);
+  @POST("getAnimals")
+  Single<List<Animal>> getAnimals(@Field("key") String key);
 
+  class InstanceHolder {
 
+    private static final AnimalService INSTANCE;
+
+    static {
+      Gson gson = new GsonBuilder()
+          .excludeFieldsWithoutExposeAnnotation()
+          .create();
+      HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+      interceptor.setLevel(Level.BODY);
+      OkHttpClient client = new OkHttpClient.Builder()
+          .addInterceptor(interceptor)
+          .build();
+      Retrofit retrofit = new Retrofit.Builder()
+          .baseUrl(BuildConfig.BASE_URL)
+          .addConverterFactory(GsonConverterFactory.create(gson))
+          .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+          .client(client)
+          .build();
+
+      INSTANCE = retrofit.create(AnimalService.class);
+    }
+  }
 }
